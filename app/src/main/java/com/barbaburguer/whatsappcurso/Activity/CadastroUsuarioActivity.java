f28f187d6defebb6d362cdd38c9c81715e2eeaca
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-12-07 17:51:52 -0300

    Atualizado para utilizar login e senha
    Foi colocado também uma área para login e transportar o usuário para a área de cadastro(ainda não funcionando)

---
+++ app/src/main/java/com/barbaburguer/whatsappcurso/Activity/CadastroUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.barbaburguer.whatsappcurso.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.barbaburguer.whatsappcurso.R;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
