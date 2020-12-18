8b44605fbd091fd1c4e55cb6fa2e651f3be333e8
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-12-18 16:01:50 -0300

    Cadasto de Usuário feito

--- app/src/main/java/com/barbaburguer/whatsappcurso/Activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/barbaburguer/whatsappcurso/Activity/CadastroUsuarioActivity.java
@@ -1,16 +1,67 @@
 package com.barbaburguer.whatsappcurso.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.barbaburguer.whatsappcurso.Config.ConfiguracaoFirebase;
+import com.barbaburguer.whatsappcurso.Modelo.Usuario;
 import com.barbaburguer.whatsappcurso.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    private EditText nome,email,senha;
+    private Button botãoCadastrar;
+    private Usuario usuario;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        nome = findViewById(R.id.edit_cadastrado_nome);
+        email = findViewById(R.id.edit_cadastrado_email);
+        senha = findViewById(R.id.edit_cadastrado_senha);
+        botãoCadastrar = findViewById(R.id.bt_cadastrar);
+
+        botãoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setNome(nome.getText().toString());
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                cadastrarUsuario();
+
+            }
+        });
     }
+    public void cadastrarUsuario(){
+        auth = ConfiguracaoFirebase.getAutenticacao();
+        auth.createUserWithEmailAndPassword(
+                usuario.getEmail(),usuario.getSenha()
+        ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(CadastroUsuarioActivity.this, "Sucesso ao cadsatrar usuário", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(CadastroUsuarioActivity.this, "Erro ao cadastrar usuário", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/barbaburguer/whatsappcurso/Config/ConfiguracaoFirebase.java
+++ app/src/main/java/com/barbaburguer/whatsappcurso/Config/ConfiguracaoFirebase.java
@@ -1,11 +1,13 @@
 package com.barbaburguer.whatsappcurso.Config;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class ConfiguracaoFirebase {
 
     private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth autenticacao;
 
     //recuperar a instancia do Firebase
     public static DatabaseReference getFirebase(){
@@ -14,6 +16,13 @@ public final class ConfiguracaoFirebase {
         }
 
         return referenciaFirebase;
+    }
+    public static FirebaseAuth getAutenticacao(){
+        if(autenticacao == null){
+            autenticacao = FirebaseAuth.getInstance();
+        }
+
+        return autenticacao;
     }
 
 }
