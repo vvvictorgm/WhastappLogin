a74d3b778fe568f19d293d0a9d302a1fde945be9
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-12-18 17:09:00 -0300

    Arrumado erro
    Adicionado @exclude para garantir que só seja enviado ao Firebase os dados não duplicados

--- app/src/main/java/com/barbaburguer/whatsappcurso/Modelo/Usuario.java
+++ app/src/main/java/com/barbaburguer/whatsappcurso/Modelo/Usuario.java
@@ -1,6 +1,8 @@
 package com.barbaburguer.whatsappcurso.Modelo;
 import com.barbaburguer.whatsappcurso.Config.ConfiguracaoFirebase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
 public class Usuario {
     private String id,nome,email,senha;
 
@@ -12,7 +14,7 @@ public class Usuario {
         reference.child("usuários").child(getId()).setValue(this);
 
     }
-
+    @Exclude
     public String getId() {
         return id;
     }
@@ -36,7 +38,7 @@ public class Usuario {
     public void setEmail(String email) {
         this.email = email;
     }
-
+    @Exclude
     public String getSenha() {
         return senha;
     }
